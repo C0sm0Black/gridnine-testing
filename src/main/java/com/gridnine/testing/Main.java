72b768df7511f3e74f1ee85f8a52099bfb9bd7aa
CosmoBlack <soulker_mozyr@yahoo.com>
2023-08-31 21:25:17 +0200

    Initial commit

---
+++ src/main/java/com/gridnine/testing/Main.java
@@ -0,0 +1,15 @@
+package com.gridnine.testing;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Flight> flightList = FlightBuilder.createFlights();
+
+        System.out.println(flightList);
+
+    }
+
+}
